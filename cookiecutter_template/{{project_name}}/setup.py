0674e1bb628fc344299a865221989e821bcbb6a3
Lockefox <locke.renard@gmail.com>
2017-12-12 12:11:37 -0800

    filling out cookiecutter template

--- cookiecutter_template/{{project_name}}/setup.py
+++ cookiecutter_template/{{project_name}}/setup.py
@@ -3,7 +3,7 @@ from codecs import open
 
 from setuptools import setup
 
-__package_name__ = 'dummy-env-prep'
+__package_name__ = '{{cookiecutter.project_name}}'
 __version__ = '1.0.0'
 
 with open('README.rst', 'r', 'utf-8') as fh:
@@ -32,12 +32,12 @@ Includes
 
 setup(
     name=__package_name__,
-    description='A handy template for including requirements in an environment',
+    description='{{cookiecutter.project_brief}}',
     long_description=make_readme(),
     version=__version__,
-    author='John Purcell',
-    author_email='jpurcell.ee@gmail.com',
-    url='https://github.com/lockefox/' + __package_name__,
+    author='{{cookiecutter.author}}',
+    author_email='{{cookiecutter.email}}',
+    url='https://github.com/{{cookiecutter.github_handle}}/' + __package_name__,
     license='MIT',
     classifiers=[
         'Programming Language :: Python :: 3.5',
@@ -45,7 +45,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Topic :: Software Development :: Build Tools',
     ],
-    keywords='environment setup requirements packaging automation',
+    keywords='{{cookiecutter.keywords}}',
     package_data={
         '': ['LICENSE', 'README.rst', 'requirements.txt']
     },
